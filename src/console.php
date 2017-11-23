10a4b5d0b5d730f186522f7bcb17b5b4644be1ca
Drunken-Dragon <janziolo@gmail.com>
2017-11-23 12:28:32 +0100

    removed setDescription from console.php

--- src/console.php
+++ src/console.php
@@ -10,6 +10,5 @@ $console = new Application('My Silex Application', 'n/a');
 $console->getDefinition()
         ->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
 $console->setDispatcher($app['dispatcher']);
-$console->setDescription('My command description');
 
 return $console;
