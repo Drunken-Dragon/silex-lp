f6d0588237160dda27b0430a283d898ece6856f0
Drunken-Dragon <janziolo@gmail.com>
2017-11-28 08:45:11 +0100

    added form and translate dependencies. Registered form services

--- src/app.php
+++ src/app.php
@@ -5,6 +5,7 @@ use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\FormServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
@@ -12,7 +13,14 @@ $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
 $app->register(new Silex\Provider\SessionServiceProvider());
-$app->register(new Silex\Provider\FormServiceProvider());
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\LocaleServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), [
+    'locale_fallbacks' => ['en'],
+]);
+$app->register(new Silex\Provider\TranslationServiceProvider(), [
+    'translator.domains' => [],
+]);
 $app->register(new \Silex\Provider\DoctrineServiceProvider(), [
     'db.options' => [
         'driver' => 'pdo_mysql',
