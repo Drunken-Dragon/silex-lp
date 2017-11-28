7978f88ee06bf92da3219c689f414141f242a22f
Drunken-Dragon <janziolo@gmail.com>
2017-11-28 08:51:37 +0100

    added first form

--- src/controllers.php
+++ src/controllers.php
@@ -4,6 +4,43 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Silex\Provider\FormServiceProvider;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+$app->match('/form', function (Request $request) use ($app) {
+    // some default data for when the form is displayed the first time
+    $data = array(
+        'name' => 'Your name',
+        'email' => 'Your email',
+    );
+
+    $form = $app['form.factory']->createBuilder(FormType::class, $data)
+        ->add('name')
+        ->add('email')
+        ->add('billing_plan', ChoiceType::class, array(
+            'choices' => array('free' => 1, 'small business' => 2, 'corporate' => 3),
+            'expanded' => true,
+        ))
+        ->add('submit', SubmitType::class, [
+            'label' => 'Save',
+        ])
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+
+        // do something with the data
+
+        // redirect somewhere
+        return $app->redirect('...');
+    }
+
+    // display the form
+    return $app['twig']->render('form.twig', array('form' => $form->createView()));
+});
 
 $app->get('/', function () use ($app) {
     if (($app['session']->get('is_logged')) === 1) {
